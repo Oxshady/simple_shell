9b8f84209bc75eca0b28a3ed7a83c28ba8e4b4b8
youssef ahmed <116092909+youssef3092004@users.noreply.github.com>
2024-02-22 23:17:38 +0200

    Update Readline.c

--- Readline.c
+++ Readline.c
@@ -1,28 +1,31 @@
 #include "main.h"
-/**
- * Read_line - function that read data from the user (standard input)
- * @input: double pointer to the string to be read
- * @size: the size of input string
- * Return: 0 if EOF EXIT-FAIULE if error number of chars if end successfully
+/
+ 
+Read_line - function that read data from the user (standard input)
+@input: double pointer to the string to be read
+@size: the size of input string
+Return: 0 if EOF EXIT-FAIULE if error number of chars if end successfully
 */
-ssize_t Read_line(char **input, size_t *size)
+ssize_t Read_line(char input, size_t size)
 {
-	ssize_t Read = getline(input, size, stdin);
+    ssize_t Read = getline(input, size, stdin);
+    if (Read == -1)
+    {
+        if (feof(stdin))
+        {
+            free(input);
+            input = NULL;
+            exit(0);
+        }
+        else
+        {
+            free(input);
+            input = NULL;
+            exit(EXIT_FAILURE);
+        }
+    }
+    if (input[0] = '\n')
+        return 2;
+    return (Read);
 
-	if (Read == -1)
-	{
-		if (feof(stdin))
-		{
-			free(input);
-			input = NULL;
-			exit(0);
-		}
-		else
-		{
-			free(input);
-			input = NULL;
-			exit(EXIT_FAILURE);
-		}
-	}
-	return (Read);
 }
