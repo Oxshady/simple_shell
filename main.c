6bf273ed3569b3fbd717d22015fc4bd5e2fa3067
youssef <youssefahmedabdelkader25@gmail.com>
2024-02-22 21:25:28 +0200

    g

---
+++ main.c
@@ -0,0 +1,46 @@
+#include "main.h"
+
+/**
+ * main - simple shell
+ * @argv: vector argument
+ * @ac: counter argument
+ * @environment: environment to find the path
+ * Return: 0
+ */
+
+int main(__attribute__((unused))int argc, char **argv, char **env)
+{
+char *usr_input;
+char **tokens;
+int status = 0;
+itn com_count = 0;
+
+while (1)
+{
+
+// user_input  = getline
+
+if (usr_inpt == NULL)
+{
+    if (isatty(STDIN_FILENO))
+{
+    write(STDOUT_FILENO, "$ ", 2);
+}
+return (status);
+}
+com_count++;
+
+// tokens = _strtok(user_input)
+
+if (!tokens)
+{
+continue;
+}
+
+/*
+ enviromet path
+ chek the command in the path or not
+ exce the command
+*/
+}
+}
