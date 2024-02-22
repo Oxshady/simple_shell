46b0bbdb241312d92f1e5c4dfaa91a076b867813
Oxshady <shadymahmod55@gmail.com>
2024-02-22 21:07:45 +0200

    adding the prototype of readline and the header files

---
+++ main.h
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#ifndef MAIN_H
+#define MAIN_H
+ssize_t Read_line(char **input, size_t *size);
+#endif
