500b5964b444e4a2e3f8759cbc2af73f69187e6f
Oxshady <shadymahmod55@gmail.com>
2024-02-23 15:39:40 +0200

    handle memory leaks

--- tokenization.c
+++ tokenization.c
@@ -7,12 +7,14 @@ void tokenize(char *inp, int size, char ***vector)
 	char *inp_copy = NULL;
 	int i = 0;
 
+	if (inp != NULL && size > 0)
+	{
 	inp_copy = (char *) malloc(sizeof(char) * size);
 	if (inp_copy == NULL)
 	{
-		free(inp_copy);
-		return 3;
+		perror("error in memory allocation for input_copy");
 	}
+
 	strcpy(inp_copy,inp);
 
 	token = strtok(inp, delim);
@@ -27,16 +29,29 @@ void tokenize(char *inp, int size, char ***vector)
 	(*vector) = (char **) malloc(sizeof(char *) * (token_counter + 1));
 
 	if ((*vector) == NULL)
-		return(10);
+	{
+		perror("error for allocate memory to vector");
+		free(inp_copy);
+	}
 	token = strtok(inp_copy,delim);
 	while (token)
 	{
 		(*vector)[i] = (char *) malloc((strlen(token) + 1) * sizeof(char));
 		if ((*vector)[i] == NULL)
-			return 80;
+		{
+			perror("failed to allocate for individual st");
+			for(size_t ii = 0; ii < i; ii++)
+			{
+				free((*vector)[ii]);
+			}
+			free(*vector);
+			free(inp_copy);
+		}
 		strcpy((*vector)[i],token);
 		i++;
 		token = strtok(NULL,delim);
 	}
 	(*vector)[i] = NULL;
+	free(inp_copy);
+	}
 }
