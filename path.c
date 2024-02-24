65efbd8ca582016da1294f3d030ac7943e2f7031
Oxshady <shadymahmod55@gmail.com>
2024-02-24 15:20:59 +0200

    updating to resolve error

--- path.c
+++ path.c
@@ -1,23 +1,22 @@
 #include "main.h"
 extern char **__environ;
-char *_path(void)
+char *_path(char **str)
 {
     size_t i = 0, len_path = 0;
-    char *path = NULL;
 
     for (i = 0; __environ[i] != NULL; i++)    
     {
         if (strncmp(__environ[i], "PATH=", 5) == 0)
         {
             len_path = (strlen(__environ[i]) - 5);
-            path = (char *) malloc(sizeof(char) * (len_path + 1));
-            if (path == NULL)
+            *(str) = (char *) malloc(sizeof(char) * (len_path + 1));
+            if ((*str) == NULL)
                 return NULL;
-            strcpy(path,((__environ[i]) + 5));
+            strcpy(*(str),((__environ[i]) + 5));
             break;
         }
         if(__environ[i]== NULL)
             return (NULL);
     }
-    return (path);
+
 }
