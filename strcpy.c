f03cc6674d152dfe5d1f561b4ab3ae5e3844075f
Oxshady <shadymahmod55@gmail.com>
2024-02-25 17:20:35 +0200

    done

--- strcpy.c
+++ strcpy.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
- * _strncpy - function that copies a string
- * @dest:string to copy to
- * @src:string to copy from
- * @n: number of chars to copy
- * Return: address of first char in dest
+ * _strcpy - copy the string from source to distination
+ * Description: function that copies the string pointed to by src
+ * including the terminating null byte (\0), to the buffer pointed to by dest
+ * @dest: array of characters to copy to
+ * @src: string to copy from
+ * Return: address of the first location of the dest
 */
-char *_strncpy(char *dest, char *src, int n)
+char *_strcpy(char *dest, char *src)
 {
-	int i;
+	int i = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
+	while (src[i] != 0)
 	{
 		dest[i] = src[i];
-	}
-	while (i < n)
-	{
-		dest[i] = '\0';
 		i++;
 	}
+	dest[i] = src[i];
 	return (dest);
 }
