809b26085d94a5c7445a812d3f51dd139e8101b8
Oxshady <shadymahmod55@gmail.com>
2024-02-27 23:27:55 +0200

    100% done

--- exit.c
+++ exit.c
@@ -1,22 +1,26 @@
 #include "main.h"
-
+/**
+ * _exitt - function that exit code with stat
+ * @vect: pointer to pointer to characters --> tokens
+ * Return: status
+*/
 int _exitt(char **vect)
 {
-    if (vect == NULL || vect[0] == NULL)
-        return -1; 
+	char *x = "exit";
 
-    char *x = "exit";
-    int exit_code = 0;
+	if (vect == NULL || vect[0] == NULL)
+		return (-1);
+	int exit_code = 0;
 
-    if (strncmp(vect[0], x, strlen(x)) == 0)
-    {
-        if (vect[1] != NULL)
-        {
-            exit_code = atoi(vect[1]);
-            return exit_code; 
-        }
-        else
-            return 0; 
-    }
-    return (-1); 
+	if (strncmp(vect[0], x, strlen(x)) == 0)
+	{
+		if (vect[1] != NULL)
+		{
+			exit_code = atoi(vect[1]);
+			return (exit_code);
+		}
+		else
+			return (0);
+	}
+	return (-1);
 }
