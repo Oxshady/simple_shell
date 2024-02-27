b2ecd05564cf938dea99a3ca9af875d05d606fe0
youssef <youssefahmedabdelkader25@gmail.com>
2024-02-27 17:00:17 +0200

    d

---
+++ Print_error.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+void Print_error(char *argv, int *counter_line, char *command)
+{
+    write(STDIN_FILENO, argv, _strlen(argv));
+    write(STDIN_FILENO, ": ", 2);
+    write(STDIN_FILENO, counter_line, _strlen(counter_line));
+    write(STDIN_FILENO, ": ", 2);
+    write(STDIN_FILENO, command, _strlen(command));
+    write(STDIN_FILENO, ": not found", 11);
+}
+
+
+
--- exit.c
+++ exit.c
@@ -1,7 +1,27 @@
-// #include "main.h"
+#include "main.h"
 
-// void _exit(argv,status) 
-// {
-//      printf("Exiting the shell with status %d\n", status);
-//     exit(status);
-// }
+void _exitt(char **input)
+{
+    char *x = "exit";
+    char **asd;
+    int i = 0;
+    int y = 0;
+
+    if(strncmp(input, x, 4) == 0)
+    {
+        asd[i] = strdup(strtok(input, " "));
+        while (asd != NULL)
+        {
+            asd[i] = strdup(strtok(NULL, " "));
+            i++;
+        }
+        y = _atoi(asd[1]);
+        _exit(y);
+        free(asd);
+    } 
+}
+int main(void)
+{
+    char **input;
+    scanf(&input);
+}
