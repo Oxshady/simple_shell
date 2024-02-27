3f75726802411284eb15948834f94f2eff510dee
Oxshady <shadymahmod55@gmail.com>
2024-02-28 00:42:15 +0200

    last version

--- main.h
+++ main.h
@@ -26,5 +26,10 @@ int _exitt(char **vect);
 void Print_error(char *argv, int *counter_line, char *command);
 char *_itoa(int num);
 void revstr(char *buf, int i);
+char *createCommandPath(char *path, char *command);
+int executeWithPath(char **vect);
+int executeFromPaths(char **vect, char **paths);
+int containsSlash(char *str);
+int _execve(char **vect, char **paths);
 #endif
 
