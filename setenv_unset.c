2261b5a0e4e2e95c77dbced19cb89dceb31d7b0a
Oxshady <shadymahmod55@gmail.com>
2024-02-27 16:58:08 +0200

    updating....

---
+++ setenv_unset.c
@@ -0,0 +1,47 @@
+#include "main.h"
+int setenv_unset(char **vect,int val)
+{
+	char *line = NULL;
+    char *VARNAME = NULL;
+    char *VALUE = NULL;
+    if (vect == NULL || vect[1] == NULL) {
+        printf("Invalid input\n");
+        return -1;
+    }
+	line = strdup(vect[1]);
+	VARNAME = strtok(line, " =");
+    if (VARNAME == NULL) {
+        printf("Invalid input\n");
+        return -1;
+    }
+	if (val == 3)
+	{
+		free(line);
+		unsetenv(VARNAME);
+		printf(" variable %s unsettt",VARNAME);
+		return(4);
+	}
+	
+    VALUE = strtok(NULL, "");
+
+    if (VALUE[0] == '"' && VALUE[strlen(VALUE) - 1] == '"') {
+        VALUE[strlen(VALUE) - 1] = '\0';
+        VALUE++;
+    }
+
+    if (setenv(VARNAME, VALUE, 0) == 0) {
+        printf("Environment variable %s set to %s\n", VARNAME, VALUE);
+    } else {
+        perror("setenv");
+        return -1;
+    }
+
+    return 0;
+}
+
+int main(void)
+{
+	char *ve[3] = {"export","MY_VAR=\"HOME\""};
+	setenv_unset(ve,2);
+	return 0;
+}
