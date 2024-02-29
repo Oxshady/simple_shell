ab6f26fae378ed60426b530bcafd8638ae5e800c
Oxshady <shadymahmod55@gmail.com>
2024-02-29 03:25:28 +0200

    done

--- getline.c
+++ getline.c
@@ -7,5 +7,43 @@
 */
 size_t _getline(char **inp, size_t *size)
 {
-	return(getline(inp,size,stdin));
+	ssize_t bytes;
+	*size = 1024;
+
+	*inp = (char *)calloc(*size, sizeof(char));
+	if (*inp == NULL)
+	{
+		perror("Memory allocation failed");
+		return (-1);
+	}
+	bytes = read(STDIN_FILENO, *inp, *size);
+	if (bytes == -1)
+	{
+		perror("read"), free(*inp);
+		return (-1);
+	}
+	if (bytes == 0)
+	{
+		free(*inp);
+		return (0);
+	}
+	if ((size_t)bytes == *size)
+	{
+		*size *= 2, *inp = (char *)realloc(*inp, *size * sizeof(char));
+		if (*inp == NULL)
+		{
+			perror("Memory reallocation failed");
+			return (-1);
+		}
+		bytes += read(STDIN_FILENO, *inp + bytes, *size - bytes);
+	}
+	*size = bytes, *inp = realloc(*inp, *size);
+
+	if (*inp == NULL)
+	{
+		perror("Memory reallocation failed");
+		return (-1);
+	}
+	(*inp)[bytes] = '\0';
+	return (bytes);
 }
--- main.c
+++ main.c
@@ -1,55 +1,52 @@
 #include "main.h"
 
-int main(__attribute__((unused)) int argc, char **argv, char **envp) {
-    char *usr_input = NULL, **tokens = NULL;
-    int input_stat = 0, pid = 0;
-    size_t size = 1024;
-    int sa = 0, status = 0, shell_stat = 0, line_counter = 0;
+/**
+* main - Simple shell
+* @argc: Argument count
+* @argv: Argument vector
+* @envp: Environment variables
+* Return: Always 0
+*/
 
-    while (1) {
-        shell_stat = shell_prompt(), line_counter++;
-        if (shell_stat == 1 || shell_stat == 0) {
-            input_stat = _getline(&usr_input, &size);
-            if (usr_input[0] == '\n') {
-                continue;
-            }
-            if (input_stat == 0) {
-                free(usr_input);  // Free usr_input before exit
-                _exit(0);
-            } else if (input_stat == -1) {
-                free(usr_input);  // Free usr_input before exit
-                break;
-            } else {
-                tokens = tokenize(usr_input);
-                status = handle(tokens, &status, &sa, envp);
-                free(usr_input);  // Free usr_input here
-                if (status == 0) {
-                    free_mem(&tokens);
-                    _exit(0);
-                } else if (status == -1) {
-                    free_mem(&tokens);
-                    exit(-1);
-                } else if (status == -2) {
-                    free_mem(&tokens);
-                    continue;
-                } else if (status > 0) {
-                    free_mem(&tokens);
-                    _exit(status);
-                } else if (status == -3) {
-                    pid = Create_process();
-                    if (pid == 0) {
-                        if (_execve(tokens, tokenize(_path(envp))) == -1) {
-                            free_mem(&tokens);
-                        }
-                        Print_error(argv[0], &line_counter, tokens[0]);
-                        exit(0);
-                    } else if (pid > 0) {
-                        free_mem(&tokens);
-                        wait(NULL);
-                    }
-                }
-            }
-        }
-    }
-    return 0;
+int main(__attribute__((unused)) int argc, char **argv, char **envp)
+{char *usr_input = NULL, **tokens = NULL;
+int input_stat = 0, pid = 0;
+size_t size = 0;
+int sa = 0, status = 0, shell_stat = 0, line_counter = 0;
+
+while (1)
+{shell_stat = shell_prompt(), line_counter++;
+if (shell_stat == 1)
+{
+input_stat = _getline(&usr_input, &size);
+if (usr_input[0] == '\n')
+continue;
+if (input_stat == 0)
+_exit(0);
+else if (input_stat == -1)
+break;
+else
+{tokens = tokenize(usr_input), status = handle(tokens, &status, &sa, envp);
+if (status == 0 || status == -1)
+_exit(0);
+else if (status == -2)
+continue;
+else if (status > 0)
+_exit(status);
+else if (status == -3)
+{pid = Create_process();
+if (pid == 0)
+{
+	if (_execve(tokens, tokenize(_path(envp))) == -1)
+{};
+Print_error(argv[0], &line_counter, tokens[0]);
+exit(0);
+}
+else if (pid > 0)
+wait(NULL);
+}}}
+else
+return (-1);
+}
+return (0);
 }
